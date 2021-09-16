c183122a5e9c6f21062b77e9773aa524105ca011
choi <choi.dahye424@gmail.com>
2021-09-17 08:24:58 +0900

    스프링부트 몽고디비 연동

---
+++ src/main/java/com/cos/mongoapp/domain/Board.java
@@ -0,0 +1,17 @@
+package com.cos.mongoapp.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "board")
+public class Board {
+	
+	@Id
+	public String _id;
+	private String title;
+	private String content;
+	
+}
